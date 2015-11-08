6a5323aecc0e5bd4a36e129e8f3445410b144c10
BillChabzz <chaby9.8@gmail.com>
2015-11-08 12:15:29 +0300

    fixed Parse API and tested working database

--- app/src/main/java/nata/fragz/com/natanoti/Journalcreate.java
+++ app/src/main/java/nata/fragz/com/natanoti/Journalcreate.java
@@ -8,9 +8,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.github.clans.fab.FloatingActionButton;
 import com.github.clans.fab.FloatingActionMenu;
+import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class Journalcreate extends AppCompatActivity {
 
@@ -18,6 +21,9 @@ public class Journalcreate extends AppCompatActivity {
     private FloatingActionButton fab1;
     private FloatingActionButton fab2;
     private FloatingActionButton fab3;
+    String journalTitle;
+    String journalCompose;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,31 @@ public class Journalcreate extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final Button btn = (Button)findViewById(R.id.button);
+        //Parse API codes
+        // Enable Local Datastore.
+        Parse.enableLocalDatastore(this);
+
+        Parse.initialize(this, "GDqpaudUzxIubXTzJTXko0i062wWxqLK1AJSzD2K", "KHiJQsUdQYQcZVtxTCuFM6MmbQGKcIGEW1idXmC0");
+
+        //Parse test
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+
+        final EditText jTitle=(EditText)findViewById(R.id.journal_title);
+        final EditText jCompose=(EditText)findViewById(R.id.journal_compose);
+        final Button btnSave = (Button)findViewById(R.id.btn_save_journal);
         menu1 = (FloatingActionMenu)findViewById(R.id.menu1);
         fab1 =(com.github.clans.fab.FloatingActionButton)findViewById(R.id.fab1);
         fab2 =(com.github.clans.fab.FloatingActionButton)findViewById(R.id.fab2);
         fab3 =(com.github.clans.fab.FloatingActionButton)findViewById(R.id.fab3);
+
+        //Text to string
+        journalTitle= jTitle.getText().toString().trim();
+        journalCompose=jCompose.getText().toString().trim();
+
+
+        //Click listeners
 
         fab1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,6 +75,14 @@ public class Journalcreate extends AppCompatActivity {
             public void onClick(View v) {
                 Intent create= new Intent(Journalcreate.this,Journalcreate.class);
                 startActivity(create);
+            }
+        });
+
+        //journal save button
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
             }
         });
     }
