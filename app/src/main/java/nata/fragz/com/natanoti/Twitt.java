5fc6d3ad557859cb32987657e2014b5fc56a8027
martmwangi <luthermwangi2@gmail.com>
2015-11-10 10:48:07 +0300

    Testing drawables collection

---
+++ app/src/main/java/nata/fragz/com/natanoti/Twitt.java
@@ -0,0 +1,74 @@
+package nata.fragz.com.natanoti;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+import android.widget.Toolbar;
+
+import io.fabric.sdk.android.Fabric;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
+import com.twitter.sdk.android.core.*;
+import com.twitter.sdk.android.core.identity.*;
+
+
+public class Twitt extends AppCompatActivity {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "67L1nigZNjhmqF6T4nXgzZ4go";
+    private static final String TWITTER_SECRET = "wsBg9wemjh9HJ8tXWDVRdj6c4VUhSgW8SsT8GbRjsPpJXWkgLD";
+    private TwitterLoginButton loginButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+        setContentView(R.layout.activity_twitt);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+
+
+        loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
+        loginButton.setCallback(new Callback<TwitterSession>() {
+            @Override
+            public void success(Result<TwitterSession> result) {
+                // The TwitterSession is also available through:
+                // Twitter.getInstance().core.getSessionManager().getActiveSession()
+
+                TwitterSession session = result.data;
+                // TODO: Remove toast and use the TwitterSession's userID
+                // with your app's user model
+//                String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
+//                Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+                tojournal();
+//                Intent loki = new Intent(Twitt.this,Journal.class);
+//                startActivity(loki);
+//                startActivity(new Intent(getBaseContext(),Journal.class));
+            }
+            @Override
+            public void failure(TwitterException exception) {
+                tojournal();
+                Log.d("TwitterKit", "Login with Twitter failure", exception);
+            }
+            private void tojournal(){
+                Intent loki = new Intent(Twitt.this,Natahome.class);
+                startActivity(loki);
+            }
+        });
+
+
+
+
+
+
+
+    }
+
+}
