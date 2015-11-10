b3e120950d49b8c9901468becd0f7490efed8fe5
BillChabzz <chaby9.8@gmail.com>
2015-11-10 14:19:46 +0300

    fixed recycler view cards on journal tabs

--- app/src/main/java/nata/fragz/com/natanoti/Journal_public.java
+++ app/src/main/java/nata/fragz/com/natanoti/Journal_public.java
@@ -10,6 +10,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class Journal_public extends Fragment {
     RecyclerView mRecyclerView;
@@ -18,16 +19,20 @@ public class Journal_public extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.journal_public, container, false);
+        View view = inflater.inflate(R.layout.journal_public, container, false);
+
+        FrameLayout frameLayout = (FrameLayout) view.findViewById(R.id.card_frame);
 
 
-       // mRecyclerView = (RecyclerView)view.findViewById(R.id.recycler_view);
-       // mRecyclerView.setHasFixedSize(true);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
 
-      //  mLayoutManager = new LinearLayoutManager(this);
-      //  mRecyclerView.setLayoutManager(mLayoutManager);
+        mLayoutManager = new LinearLayoutManager(getActivity().getBaseContext());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
 
-       // mAdapter = new Journaladapter();
-       // mRecyclerView.setAdapter(mAdapter);
+        mAdapter = new Journaladapter();
+        mRecyclerView.setAdapter(mAdapter);
+        return view;
     }
 }
